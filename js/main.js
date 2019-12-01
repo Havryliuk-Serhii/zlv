a1e1793fd205f4ca3978a8429bd50a4ac85c7427
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2019-12-01 16:42:40 +0200

    add sticky navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+(function($) {
+	"use strict"
+	// On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+		wScroll > 1 ? $('#nav').addClass('bg-dark') : $('#nav').removeClass('bg-dark');
+		});
+	
+})(jQuery);
+
+	
 
